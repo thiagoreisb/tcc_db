3c5ae4119789da52b1e6360983e45453d85bc95d
thiagoreisb <thiagoreisb@gmail.com>
2019-04-28 14:19:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TCC Database
+It's a simple app to manage the TCC's database
+
---
+++ index.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+'use strict';
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const pg = require('pg');
+
+const app = new express();
+const pool = new pg.Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT
+});
+
+// register JSON parser middlewear
+app.use(bodyParser.json());
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Welcome to the DB app!');
+});
+
+app.get('/db', (req, res) => {
+    pool.connect(function (err, client, done) {
+        if (err) {
+            res.send('Cannot connect to db! ' + err);
+        }
+        client.query('SELECT * FROM pg_catalog.pg_tables WHERE schemaname = \'public\';', function (err, result) {
+            done();
+            if (err) {
+                res.send('Cannot retrieve data! ' + err);
+            }
+            res.send(result.rows);
+        })
+    });
+});
+
+// start server on port 3000
+app.listen(3000, () => {
+    console.log('Server up!');
+});
