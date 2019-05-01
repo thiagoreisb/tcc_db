9e6940b7754ee8cbc3124993079316d93ecfa65d
thiagoreisb <thiagoreisb@gmail.com>
2019-05-01 15:32:13 -0300

    testing pg connection2

--- index.js
+++ index.js
@@ -8,11 +8,8 @@ const pg = require('pg');
 
 const app = new express();
 const pool = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 });
 
 // register JSON parser middlewear
@@ -28,7 +25,7 @@ app.get('/db', (req, res) => {
         if (err) {
             res.send('Cannot connect to db! ' + err);
         }
-        client.query('SELECT * FROM pg_catalog.pg_tables WHERE schemaname = \'public\';', function (err, result) {
+        client.query('SELECT table_schema, table_name FROM information_schema.tables;', function (err, result) {
             done();
             if (err) {
                 res.send('Cannot retrieve data! ' + err);
